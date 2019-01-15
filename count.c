fab38b4b36e9ade86c7e51a4e7821fd00cedadce
pajaka <ajaka.3@buckeyemail.osu.edu>
2019-01-15 09:12:00 -0500

    Upload working count.c

--- count.c
+++ count.c
@@ -1,11 +1,12 @@
 /* Author: Petra Ajaka */
-/* Compile: gcc count.
+/* Compile: make
 /* Run: ./count <input-filename> <search-string> <output-filename> */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 const int MAX_BUFFSIZE = 100;
+const int CHUNK = 20;
 
 int main(int argc, char *argv[]) {
 	int i, j;
@@ -13,7 +14,9 @@ int main(int argc, char *argv[]) {
 	FILE *output;
 	char *search;
 	char *buffer = malloc(MAX_BUFFSIZE);
-	int read, filesize, matches, ismatch, index, err;
+	int read, filesize, ismatch, index, err;
+	int matches = 0;
+	int check[CHUNK];
 	
 	if (argc != 4) {
 		printf("The number of command line arguments is incorrect. Please enter command line arguments in the format 'count <input-filename> <search-string> <output-filename>'.\n");
@@ -37,43 +40,49 @@ int main(int argc, char *argv[]) {
 		printf("Output file: %s could not be created. Please enter a correct name for the output file.\n", argv[3]);
 		exit(1);
 	}
-	
-	fseek(input, 0, SEEK_SET);
-	read = fread(buffer, 1, 100, input);
-	filesize = read;
-	while (read >= strlen(search)) {
-		for (i = 0; (i < read) && (read - i >= strlen(search)); i++) {
-			if (buffer[i] == (search[0] & 0xff)) {
-				ismatch = 1;
-				for (j = 0; j < strlen(search); j++) {
-					if (buffer[i+j] != (search[j] & 0xff) ) {
-						ismatch = 0;
-						break;
+
+	for (i = 0; i < CHUNK; i++) {
+		check[i] = -1;
+	} 
+	do {
+		read = fread(buffer, 1, sizeof(buffer), input);
+		for (i = 0; i < read; i++) {
+			filesize++;
+			ismatch = 0;
+			index = (buffer[i] == (search[0] & 0xff));
+			for (j = 0; j < CHUNK; j++) {
+				if (check[j] != -1) {
+					if (buffer[i] == (search[check[j] + 1] & 0xff)) {
+						if ((check[j] + 2) == strlen(search)) {
+							matches++;
+							check[j] = -1;
+						}
+						else {
+							check[j]++;
+						}
+					}
+					else {
+						check[j] = -1;
 					}
 				}
-				if (ismatch == 1) {
-		        		matches++;
-		        	}
-		        }
+				
+				if (check[j] == -1) {
+					if (!ismatch && index) {
+						check[j] = 0;
+						ismatch = 1;
+					}
+				}
+			}	
 		}
-		if (read < 100) {
-			break;
-		}
-        	index = -(strlen(search)) + 1;
-        	fseek(input, index, SEEK_CUR);
-        	read = fread(buffer, 1, 100, input);
-	}
-	
-	filesize = ftell(input);
-	fseek(output, 0, SEEK_SET);
+	} while (read > 0);
 	
 	fprintf(output,"Size of input file in bytes: %d\n", filesize);
 	printf("Size of input file in bytes: %d\n", filesize);
 	fprintf(output, "Number of matches for your search string: %d\n", matches);
 	printf("Number of matches for your search string: %d\n", matches);
 	
-	free(buffer);
 	free(search);
+	free(buffer);
 	err = fclose(output);
 	if (err < 0) {
 		perror("Close");
